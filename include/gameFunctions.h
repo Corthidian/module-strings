8281f6c9308de5dbb50793b19f043cea14395cfb
Corthidian <lucifer880799@gmail.com>
2023-12-21 23:19:23 +0500

    adding the prototypes to the header file

--- include/gameFunctions.h
+++ include/gameFunctions.h
@@ -1,32 +1,16 @@
 
-//TODO: define, document, and implement this function
-initializeBlankString();
+#ifndef GAME_FUNCTIONS_H
+#define GAME_FUNCTIONS_H
 
-//TODO: define, document, and implement this function
-printWithSpaces();
+#include <stdio.h>
+#include <string.h>
 
-//TODO: define, document, and implement this function
-revealGuessedLetter();
-
-//TODO: define, document, and implement this function
-checkGuess();
-
-
-//the following functions have been done for you.
-
-/**
- * Sets the game up, checks for win condition,
- * prints relevant data
- */
-void startGame(char *wordToGuess);
-
-/**
- * Clears the unix terminal of previous input
- */
+void initializeBlankString(int length, char *str);
+void printWithSpaces(const char *str);
+int revealGuessedLetter(const char *solution, char *revealed, char guessedLetter);
+int checkGuess(const char *str1, const char *str2);
+void startGame(char word[25]);
+void drawHorse(int guessedSoFar, int allowedGuesses);
 void clearScreen();
 
-/**
- * Prints part of the horse based on the ratio between
- * the two numbers.
- */
-void drawHorse(int guessedSoFar, int allowedGuesses);
+#endif 
